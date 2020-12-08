1f38650ec734beb2b5c0da62b2e708aae4117836
phaseIV <hazepiv@gmail.com>
2020-12-08 11:14:03 +0100

    restored original directory structure

--- README.md
+++ README.md
@@ -1,5 +1,4 @@
 # Blender-Hirise-DTM-Importer
 
 Modified version of the original Hirise DTM importer addon that will work with Blender 2.8+.
-
-The original addon was written by Nicholas Wolf. Due to difficulties of the multifile structure this reworked version has all code merged into one python file.
+The original addon was written by Nicholas Wolf.
---
+++ dtmimporter/__init__.py
@@ -0,0 +1,76 @@
+# This file is a part of the HiRISE DTM Importer for Blender
+#
+# Copyright (C) 2017 Arizona Board of Regents on behalf of the Planetary Image
+# Research Laboratory, Lunar and Planetary Laboratory at the University of
+# Arizona.
+#
+# This program is free software: you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by the Free
+# Software Foundation, either version 3 of the License, or (at your option)
+# any later version.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+# or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+# for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+"""A HiRISE DTM Importer for Blender"""
+
+import bpy
+
+from .ui import importer
+from .ui import terrainpanel
+
+bl_info = {
+    "name": "HiRISE DTM Importer",
+    "author": "Nicholas Wolf (nicwolf@pirl.lpl.arizona.edu) / phaseIV",
+    "version": (0, 2, 0),
+    "blender": (2, 80, 0),
+    "license": "GPL",
+    "location": "File > Import > HiRISE DTM (.img)",
+    "description": "Import a HiRISE DTM as a mesh.",
+    "warning": "May consume a lot of memory",
+    "category": "Import-Export",
+    "wiki_url": "",  # TBD
+    "tracker_url": "",  # TBD
+    "link": "",  # TBD
+}
+
+if "bpy" in locals():
+    import imp
+    imp.reload(importer)
+    imp.reload(terrainpanel)
+
+
+def menu_import(self, context):
+    i = importer.ImportHiRISETerrain
+    self.layout.operator(i.bl_idname, text=i.bl_label)
+
+
+classes = (
+    importer.ImportHiRISETerrain,
+    terrainpanel.TerrainPanel,
+    terrainpanel.ReloadTerrain,
+)
+
+
+def register():
+    from bpy.utils import register_class
+    for cls in classes:
+        register_class(cls)
+    bpy.types.TOPBAR_MT_file_import.append(menu_import)
+
+
+def unregister():
+    from bpy.utils import unregister_class
+    for cls in reversed(classes):
+        unregister_class(cls)
+    bpy.types.TOPBAR_MT_file_import.remove(menu_import)
+
+
+if __name__ == '__main__':
+    register()
+
